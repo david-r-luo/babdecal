db3bb2e77937ea12ddd77e85be7c4358d525db50
David Luo <davidluo@berkeley.edu>
2017-09-15 00:07:49 -0700

    Added ex2

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -2,9 +2,14 @@ pragma solidity ^0.4.16;
 
 
 contract Fibonacci {
-	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
+
+	function calculate(uint position) returns (uint) {
+	    
+		if (position < 2) {
+			return position; 
+		}
+
+		return calculate(position - 1) + calculate(position - 2);
 	}
 
-	/* Add a fallback function to prevent contract payability and non-existent function calls */
 }
